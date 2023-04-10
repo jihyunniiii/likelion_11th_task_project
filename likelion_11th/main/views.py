b4a3bc487d589c7f3ef5d641772260bc38d52e1b
jihyunniiii <jita1125@naver.com>
2023-04-10 22:22:46 +0900

    feat : HTML 페이지 띄우기 + Temlates 상속

--- likelion_11th/main/views.py
+++ likelion_11th/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def mypage(request):
+    return render(request, 'main/mypage.html')
+
+def repopage(request):
+    return render(request, 'main/repopage.html')
