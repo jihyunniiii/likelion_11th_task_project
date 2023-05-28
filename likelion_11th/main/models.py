5d9fe87a41d3ae285670b086fd45b33d640c9370
jihyunniiii <jita1125@naver.com>
2023-05-28 17:21:27 +0900

    feat : 게시물 작성자 FK로 연결 + views.py 및 템플릿 수정

--- likelion_11th/main/models.py
+++ likelion_11th/main/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    writer = models.CharField(max_length=20)
+    writer = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
     pub_date = models.DateField()
     body = models.TextField()
     feedback = models.TextField()
--- likelion_11th/main/views.py
+++ likelion_11th/main/views.py
@@ -22,36 +22,41 @@ def postdetailpage(request, id):
     return render(request, 'main/postdetail.html', {'post':post})
 
 def create(request):
-    new_post = Post()
-    new_post.title = request.POST['title']
-    new_post.writer = request.POST['writer']
-    new_post.pub_date = timezone.now()
-    new_post.body = request.POST['body']
-    new_post.feedback = request.POST['feedback']
-    new_post.good_point = request.POST['good_point']
-    new_post.image = request.FILES.get('image')
+    if request.user.is_authenticated:
+        new_post = Post()
+        new_post.title = request.POST['title']
+        new_post.writer = request.user
+        new_post.pub_date = timezone.now()
+        new_post.body = request.POST['body']
+        new_post.feedback = request.POST['feedback']
+        new_post.good_point = request.POST['good_point']
+        new_post.image = request.FILES.get('image')
 
-    new_post.save()
+        new_post.save()
 
-    return redirect('main:postdetailpage', new_post.id)
+        return redirect('main:postdetailpage', new_post.id)
+    else:
+        return redirect('accounts:login')
 
 def edit(request, id):
     edit_post = Post.objects.get(id=id)
     return render(request, 'main/edit.html', {'post':edit_post})
 
 def update(request, id):
-    update_post = Post.objects.get(id=id)
-    update_post.title = request.POST['title']
-    update_post.writer = request.POST['writer']
-    update_post.pub_date = timezone.now()
-    update_post.body = request.POST['body']
-    update_post.feedback = request.POST['feedback']
-    update_post.good_point = request.POST['good_point']
-    if request.FILES.get('image') != None:
-        update_post.image = request.FILES.get('image')
+    if request.user.is_authenticated:
+        update_post = Post.objects.get(id=id)
+        if request.user == update_post.writer:
+            update_post.title = request.POST['title']
+            update_post.pub_date = timezone.now()
+            update_post.body = request.POST['body']
+            update_post.feedback = request.POST['feedback']
+            update_post.good_point = request.POST['good_point']
+            if request.FILES.get('image') != None:
+                update_post.image = request.FILES.get('image')
 
-    update_post.save()
-    return redirect('main:postdetailpage', update_post.id)
+            update_post.save()
+            return redirect('main:postdetailpage', update_post.id)
+    return redirect('accounts:login')
 
 def delete(request, id):
     delete_post = Post.objects.get(id=id)
